9e647956d9309b7bc5729f96e55f6e78199104fd
Janus Cheng <drxaero@gmail.com>
2023-07-02 18:52:31 +0800

    BUG_FIXED - Fixed an issue in case `$CMAKE_BUILD_DIR` is not created successfully.

--- cmake_build.sh
+++ cmake_build.sh
@@ -4,7 +4,4 @@ BIN_DIR="bin"
 CMAKE_BUILD_DIR="build"
 rm -rf $BIN_DIR $CMAKE_BUILD_DIR
 mkdir $CMAKE_BUILD_DIR
-cd $CMAKE_BUILD_DIR
-cmake ../
-
-make && make install
+cd $CMAKE_BUILD_DIR && cmake ../ && make && make install
